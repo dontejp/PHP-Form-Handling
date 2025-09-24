15b813a2420d2ffff6344094912501f586c322a3
DonteJP <dontejp@gmail.com>
2025-09-23 20:39:40 -0400

    Added 3 new criteria: favorite show, why, gender

--- welcome.php
+++ welcome.php
@@ -3,6 +3,8 @@
 
     Welcome <?php echo $_POST["name"]; ?><br>
     Your email address is: <?php echo $_POST["email"]; ?><br>
+    Your favorite show is: <?php echo $_POST["tv-show"]; ?>
+    and here is why: <?php echo $_POST["reason"]; ?>
 
     </body>
 </html>
